c362f940510bb1067c16395d6f0c16768bedb3bb
sabamansoori <sabamansoori@10.0.0.102>
2023-04-10 06:35:42 -0400

    initial commit

---
+++ src/day2hwp1/Day2hwp1Demo.java
@@ -0,0 +1,29 @@
+package day2hwp1;
+
+public class Day2hwp1Demo {
+	
+	
+public static void main (String args []) {
+		
+		System.out.println ("Day 2 hw 1 stash");
+		
+	}
+	
+public void divid1 (int a, int b) {
+	
+	System.out.println (a/b);
+}
+
+
+
+public void divid2 (int a, int b) {
+	
+	System.out.println (a/b);
+
+
+}
+
+
+
+}
+
